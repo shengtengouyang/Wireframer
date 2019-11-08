43e4b731f9fc1ac750b21bdf26868672b5c390a7
Shengteng Ouyang <shengtengouyang@gmail.com>
2019-11-07 20:25:02 -0500

    change to use a new firebase project

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,14 +5,14 @@ import 'firebase/auth';
 // THIS IS USED TO INITIALIZE THE firebase OBJECT
 // PUT YOUR FIREBASE PROJECT CONFIG STUFF HERE
 var firebaseConfig = {
-    apiKey: "AIzaSyDGwHQUdc6FNs6rMo9xyznAZzTUbjHezvo",
-    authDomain: "todo-hw3-316-2571b.firebaseapp.com",
-    databaseURL: "https://todo-hw3-316-2571b.firebaseio.com",
-    projectId: "todo-hw3-316-2571b",
-    storageBucket: "todo-hw3-316-2571b.appspot.com",
-    messagingSenderId: "1084286086854",
-    appId: "1:1084286086854:web:e301f5bcd48f5412f4f1c7",
-    measurementId: "G-F7RRJ9S4J1"
+    apiKey: "AIzaSyDam5Pjqulkvcnv4e5l_pw8QlNLthiA3yM",
+    authDomain: "todo-hw3-316-301d1.firebaseapp.com",
+    databaseURL: "https://todo-hw3-316-301d1.firebaseio.com",
+    projectId: "todo-hw3-316-301d1",
+    storageBucket: "todo-hw3-316-301d1.appspot.com",
+    messagingSenderId: "496980375208",
+    appId: "1:496980375208:web:ffd32e964bb013e850349d",
+    measurementId: "G-15D1LVQPHM"
 };
 firebase.initializeApp(firebaseConfig);
 
